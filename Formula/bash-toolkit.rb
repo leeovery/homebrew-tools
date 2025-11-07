0a249480a40a7a25bb0c480a5533e779447828df
GitHub Action <action@github.com>
2025-11-07 13:55:32 +0000

    Update bash-toolkit formula to v1.0.4

--- Formula/bash-toolkit.rb
+++ Formula/bash-toolkit.rb
@@ -30,6 +30,7 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
 
     # Map versions to asset IDs (updated by automation)
     asset_ids = {
+      "1.0.4" => "313741147",
       "1.0.3" => "312447698",
       "1.0.2" => "311575871",
       "1.0.1" => "311290859",
@@ -67,9 +68,9 @@ end
 class BashToolkit < Formula
   desc "A modular bash library for terminal messaging, formatting, and user interaction"
   homepage "https://github.com/leeovery/bash-toolkit"
-  url "https://github.com/leeovery/bash-toolkit/releases/download/v1.0.3/bash-toolkit-v1.0.3.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
-  sha256 "e7c929ac092f752f0fe9febfeee68ee39e7a2871e25f372e12668e06f2f66977"
-  version "1.0.3"
+  url "https://github.com/leeovery/bash-toolkit/releases/download/v1.0.4/bash-toolkit-v1.0.4.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
+  sha256 "7af899198f6c267e6552792d60241dd0f304edaef34774410eaefe6d2a17b1d3"
+  version "1.0.4"
 
   def install
     # Install path helper binary

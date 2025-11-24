139cf35f423bf82dc923f6d81b97d94d2d2a2679
GitHub Action <action@github.com>
2025-11-24 14:26:17 +0000

    Update stitch formula to v1.2.16

--- Formula/stitch.rb
+++ Formula/stitch.rb
@@ -30,6 +30,7 @@ class GitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
 
     # Map versions to asset IDs (updated by automation)
     asset_ids = {
+      "1.2.16" => "320263468",
       "1.2.15" => "320248529",
       "1.2.14" => "320239412",
       "1.2.13" => "320234702",
@@ -80,9 +81,9 @@ end
 class Stitch < Formula
   desc "Release management CLI for coordinated feature releases"
   homepage "https://github.com/leeovery/stitch"
-  url "https://github.com/leeovery/stitch/releases/download/v1.2.15/stitch-v1.2.15.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
-  sha256 "812c76649717e324761b4d7bc179dad21cb10cb25436210d5a6532306dc82681"
-  version "1.2.15"
+  url "https://github.com/leeovery/stitch/releases/download/v1.2.16/stitch-v1.2.16.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
+  sha256 "19c33c85eb5679b7cefe2213f68929a9e9eb7a82e518ac4924a98772a50ea339"
+  version "1.2.16"
 
   depends_on "git"
   depends_on "bash-toolkit"

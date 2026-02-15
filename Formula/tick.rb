5e3de3bc5e3caad4011d2caff6381d209a9a4eaf
GitHub Action <action@github.com>
2026-02-15 17:05:21 +0000

    Update tick formula to v0.0.3

--- Formula/tick.rb
+++ Formula/tick.rb
@@ -1,16 +1,16 @@
 class Tick < Formula
   desc "Priority-based task scheduling CLI"
   homepage "https://github.com/leeovery/tick"
-  version "0.0.0"
+  version "0.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/leeovery/tick/releases/download/v#{version}/tick_#{version}_darwin_arm64.tar.gz"
-      sha256 "SHA256_DARWIN_ARM64"
+      sha256 "b74613a5b4ffb7586d07bafaf6dc5a6b019889ec8de9e7d551991609131a4ab6"
     elsif Hardware::CPU.intel?
       url "https://github.com/leeovery/tick/releases/download/v#{version}/tick_#{version}_darwin_amd64.tar.gz"
-      sha256 "SHA256_DARWIN_AMD64"
+      sha256 "334a5b3a5bf3adaf8ef47650fbd235e498149621971707a27f1ae526052cd5c8"
     end
   end
 

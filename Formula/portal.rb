3e3d4bb21b5d40e72eb63057deb596c2ad2eff46
GitHub Action <action@github.com>
2026-04-30 13:59:24 +0000

    Update portal formula to v0.3.0

--- Formula/portal.rb
+++ Formula/portal.rb
@@ -1,16 +1,16 @@
 class Portal < Formula
   desc "Interactive session picker for tmux and Zellij"
   homepage "https://github.com/leeovery/portal"
-  version "0.2.5"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/leeovery/portal/releases/download/v#{version}/portal_#{version}_darwin_arm64.tar.gz"
-      sha256 "fb3d7eaf40401db1ac3b1916c703719191d0fdf8e63e1deaf72f4903c0ad51d2"
+      sha256 "7e385d0f14fc51fbb57ccffdaa012364bb2f000a9db50b6f2416b005f2289482"
     elsif Hardware::CPU.intel?
       url "https://github.com/leeovery/portal/releases/download/v#{version}/portal_#{version}_darwin_amd64.tar.gz"
-      sha256 "f9b3160f476ab79fec6c30b44542b6ba44fa2de3f0439486dcf5f9429b0def2d"
+      sha256 "be4fd4f9b81fab3a7160fa4f50a9e17ef1e0a20193fddfec7bd827ebaae1feea"
     end
   end
 

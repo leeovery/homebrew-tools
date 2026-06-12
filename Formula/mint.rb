b91b64f096e64b25ff9475eae93acc58da3bc7d0
Lee Overy <me@leeovery.com>
2026-06-12 16:14:24 +0100

    Update mint formula to v0.0.1

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,16 +1,16 @@
 class Mint < Formula
   desc "AI-minted releases and commits"
   homepage "https://github.com/leeovery/mint"
-  version "0.0.0"
+  version "0.0.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/leeovery/mint/releases/download/v#{version}/mint_#{version}_darwin_arm64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "923bc26d97d6968b538d54d33d01cefad85ee08c59c3102842d67c06bce13041"
     elsif Hardware::CPU.intel?
       url "https://github.com/leeovery/mint/releases/download/v#{version}/mint_#{version}_darwin_amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "868ad18a3680203b10d693bf601f776954157a8d6bfd61c076e25c39de448896"
     end
   end
 

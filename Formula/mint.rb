1280226c2ce4a199552bf8539d5ccf83565c73f7
GitHub Action <action@github.com>
2026-06-17 10:44:09 +0000

    Update mint formula to v0.0.8

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,16 +1,16 @@
 class Mint < Formula
   desc "AI-minted releases and commits"
   homepage "https://github.com/leeovery/mint"
-  version "0.0.7"
+  version "0.0.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/leeovery/mint/releases/download/v#{version}/mint_#{version}_darwin_arm64.tar.gz"
-      sha256 "d289765ed4eec4b5af14e05986b9a5586b99e00e1640bbc8e542945f3dfb3620"
+      sha256 "26b434c2d94a64e4181ed6c6a0dd78faac5a75d7874cdec4cd5f027d88a7f1fd"
     elsif Hardware::CPU.intel?
       url "https://github.com/leeovery/mint/releases/download/v#{version}/mint_#{version}_darwin_amd64.tar.gz"
-      sha256 "2c225fbed37eb0eb33da52dde76d7c2d572fadc228ccbb2b0104f1e9072f6413"
+      sha256 "149c19a6271cdea06790ee69c293880a4dcd2e8fe1fd8afff2bc04f6a781f138"
     end
   end
 
